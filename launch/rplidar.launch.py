043cfd41d51934738470f6e316e0690dbcc19f7c
abdoo982 <abdullahhamed2022@gmail.com>
2024-06-13 15:12:40 -0400

    Created core URDF

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -7,11 +7,11 @@ def generate_launch_description():
     return LaunchDescription([
 
         Node(
-            package='rplidar_ros',
-            executable='rplidar_composition',
+            package='ydlidar_ros2_driver',
+            executable='ydlidar_ros2_driver_node',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-port0',
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
